8abd3523bbb7ea802a33ba738f7f969cc9357544
Maxim <maxim.maltsev@recogizer.de>
2017-08-25 15:59:56 +0200

    updated specs and package json

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,10 +1,16 @@
-let MarkerMaker = require('../main.js')
-console.log(MarkerMaker)
+'use strict'
+let MarkerMaker = require('../src/main.js')
+
+/*const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`)
+const { window } = (new JSDOM(`...`))
+const { document } = (new JSDOM(`...`)).window;*/
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    let color = MarkerMaker.colorExtractor('lightblue')
-    let result = {pinColor: 'rgb(173, 216, 230)', pinColorDark: 'rgb(113, 156, 170)'}
-    expect(color).toBe({result});
-  });
-});
+    let color = MarkerMaker('red')
+    let result = { pinColor: 'rgb(173, 216, 230)', pinColorDark: 'rgb(113, 156, 170)' }
+    expect(color).toBe({ result })
+  })
+})
